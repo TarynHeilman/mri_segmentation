5beafeb6b296d6174ab8b3f79b9302ea23d11128
Taryn Heilman <tarynheilman@gmail.com>
2018-04-29 16:45:01 -0600

    update unittests for pipeline modificaton

--- test/unittests.py
+++ test/unittests.py
@@ -19,19 +19,24 @@ class TestPipeline(unittest.TestCase):
 
     def test_create_masks(self):
         pipe = p.Pipeline('/Users/taryn/projects/mri_segmentation/data')
-        # very basic - check to make sure data and targets are same length!
-        self.assertEqual(len(pipe.target_paths), len(pipe.image_paths))
         # make sure they reference the same numbers - messy
-        img_nums = [int(fil.split('/')[-1][:-4]) for fil in pipe.image_paths]
-        mask_nums = [int(fil.split('/')[-1].split('-')[2]) for fil in pipe.target_paths]
-        self.assertEqual(set(img_nums), set(mask_nums))
+        i_img_nums = [int(fil.split('/')[-1][:-4]) for fil in pipe.i_image_paths]
+        i_mask_nums = [int(fil.split('/')[-1].split('-')[2]) for fil in pipe.i_mask_paths]
+        self.assertEqual(set(i_img_nums), set(i_mask_nums))
+        o_img_nums = [int(fil.split('/')[-1][:-4]) for fil in pipe.o_image_paths]
+        o_mask_nums = [int(fil.split('/')[-1].split('-')[2]) for fil in pipe.o_mask_paths]
+        self.assertEqual(set(o_img_nums), set(o_mask_nums))
 
     def test_read_batch_arrays(self):
         pipe = p.Pipeline('/Users/taryn/projects/mri_segmentation/data')
         # very basic - check to see if X and Y have same shape
-        sample_imgs, sample_targets = pipe.image_paths[:10], pipe.target_paths[:10]
-        x, y = pipe.read_batch_arrays(sample_imgs, sample_targets)
-        self.assertEqual(x.shape, y.shape)
+        i_sample_imgs, i_sample_targets = pipe.i_image_paths[:10], pipe.i_mask_paths[:10]
+        x1, y1 = pipe.read_batch_arrays(i_sample_imgs, i_sample_targets)
+        self.assertEqual(x1.shape, y1.shape)
+        # do this again for o contours
+        o_sample_imgs, o_sample_targets = pipe.o_image_paths[:10], pipe.o_mask_paths[:10]
+        x2, y2 = pipe.read_batch_arrays(o_sample_imgs, o_sample_targets)
+        self.assertEqual(x2.shape, y2.shape)
 
 if __name__ == '__main__':
     unittest.main()
